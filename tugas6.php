55f3af6a094a9667cd8d7cbad55e1dae066c3108
yohanes_prima <jooprima@gmail.com>
2019-10-07 10:49:57 +0700

    Tugas 6 PHP
    tugas ini dikumpulkan untuk niomic

---
+++ tugas6.php
@@ -0,0 +1,9 @@
+<?php
+$habib= 21;
+$aji=$habib+4;
+$bintan=$aji+3;
+
+echo "Usia Habib = $habib<br>";
+echo "Usia Aji = $aji<br>";
+echo "Usia Bintan = $bintan";
+ ?>
---
+++ tugas6_2.php
@@ -0,0 +1,9 @@
+<?php
+$jambu=15000;
+$kardus=2000;
+$total=($jambu*(5*6))+($kardus*2);
+
+echo "Harga Jambu = Rp $jambu /kg<br>";
+echo "Harga Kardus = Rp $kardus /pcs<br>";
+echo "Total penjualan (Dus dan Jambu) = Rp $total";
+ ?>
